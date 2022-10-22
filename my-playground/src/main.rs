b5a205dc012cd042d03221f072adbc2a02b40ea3
Peco Danajlovski <vortexmk@gmail.com>
2022-10-22 23:17:17 +0200

    error handling in rust using anyhow library

--- my-playground/src/main.rs
+++ my-playground/src/main.rs
@@ -1,18 +1,19 @@
-fn error_me(throw: bool) -> Result<(), usize> {
+use std::path::PathBuf;
+
+use anyhow::{anyhow, Context, Result};
+
+fn error_me(throw: bool) -> Result<()> {
     if throw {
-        return Err(7);
+        return Err(anyhow!("this should never be true"));
     }
+
+    std::fs::read(PathBuf::from("/foo")).context("Hey unable to do this!");
 
     return Ok(());
 }
 
-fn main() -> Result<(), usize> {
-    error_me(false)?;
-
-    let value = match error_me(false) {
-        Err(e) => return Err(e),
-        Ok(v) => v,
-    };
+fn main() -> Result<()> {
+    let value = error_me(false)?;
 
     println!("Error value: {:?}", value);
 
